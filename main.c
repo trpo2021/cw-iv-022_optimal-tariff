98a8a03618ac52983ffe9af7ff236f4b3fbd6284
BezlepkinAV <a.bezlepkin@csc.sibsutis.ru>
2021-05-14 15:01:53 +0700

    Move time.csv into csv_input

--- main.c
+++ main.c
@@ -43,7 +43,7 @@ int hrs(void)
     It = time(NULL);
     ptr = localtime(&It);
     FILE *f;
-    f = fopen("time.csv", "w");
+    f = fopen("csv_input/time.csv", "w");
     fprintf(f, "%d", ptr->tm_hour);
     fclose(f);
 }
@@ -52,7 +52,7 @@ void greeting()
 {
     FILE *f;
     struct time tm;
-    f = fopen("time.csv", "r");
+    f = fopen("csv_input/time.csv", "r");
     fscanf(f,"%d", &tm.hours);
     if(tm.hours >= 6 && tm.hours <= 11)
         printf(" Доброе утро\n");
