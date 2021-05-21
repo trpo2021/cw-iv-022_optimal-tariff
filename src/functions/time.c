cbdd0eb0b5dd46ee00e5755d112828897ed3b8a9
BezlepkinAV <a.bezlepkin@csc.sibsutis.ru>
2021-05-21 23:14:03 +0700

    Fix greeting function

--- src/functions/time.c
+++ src/functions/time.c
@@ -19,14 +19,16 @@ void greeting()
     struct time tm;
     f = fopen("../csv_input/time.csv", "r");
     fscanf(f,"%d", &tm.hours);
-    if(tm.hours >= 6 && tm.hours <= 11)
+    if(tm.hours >= 0 && tm.hours <= 5)
+        printf(" Доброй ночи\n");
+    else if(tm.hours >= 6 && tm.hours <= 11)
         printf(" Доброе утро\n");
     else if(tm.hours >= 12 && tm.hours <= 16)
         printf(" Добрый день\n");
-    else if(tm.hours >= 17 && tm.hours <= 21)
+    else if(tm.hours >= 17 && tm.hours <= 20)
         printf(" Добрый вечер\n");
-    else if(tm.hours >= 22 && tm.hours <= 5)
+    else if(tm.hours >= 21 && tm.hours <= 23)
         printf(" Доброй ночи\n");
     fclose(f);
-    remove("time.csv");
+    remove("../csv_input/time.csv");
 }
