1333d63599418d623276097d4bbb10c49bbb41a1
s4bject <artyon20026@gmail.com>
2021-05-23 14:08:50 +0700

    Make a full check in check

--- src/functions/check.c
+++ src/functions/check.c
@@ -1,11 +1,35 @@
 #include "lib.h"
 
-int check(int a, int check)
+int check(int check)
 {
-    if ((a > 0) && (a <= check))
-        return 1;
-    system("clear");
-    printf("Неверный ввод, повторите попытку\n");
-    sleep(3);
-    return 0;
+    char *start = malloc(sizeof(char) * 10);
+    int launch;
+    scanf(" %[^\n]",start);
+    if(isdigit(start[0]) == 0){
+        system("clear");
+        printf("Неверный ввод, повторите попытку\n");
+        sleep(3);
+        free(start);
+        return 10;
+    }
+    launch = start[0] - '0';
+    if(start[1] != 0){
+        system("clear");
+        printf("Неверный ввод, повторите попытку\n");
+        sleep(3);
+        free(start);
+        return 10;
+    }
+    if(launch > 0 && launch <= check){
+        free(start);
+        return launch;
+    }
+    else {
+        system("clear");
+        printf("Неверный ввод, повторите попытку\n");
+        sleep(3);
+        free(start);
+        return 10;
+    }
+    free(start);
 }
