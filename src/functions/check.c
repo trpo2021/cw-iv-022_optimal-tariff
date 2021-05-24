b49c4ee0a8b449c541c20c4ac3204569c33f95d7
s4bject <artyon20026@gmail.com>
2021-05-24 14:43:13 +0700

    Add checks for tariffs

--- src/functions/check.c
+++ src/functions/check.c
@@ -3,23 +3,49 @@
 int check(int check)
 {
     char* start = malloc(sizeof(char) * 10);
-    int launch;
+    int launch = 0;
+    ;
     scanf(" %[^\n]", start);
-    if (isdigit(start[0]) == 0) {
+    if ((isdigit(start[0]) != 0) && (isdigit(start[1]) != 0)
+        && (isdigit(start[2]) != 0) && (start[4] == 0)) {
+        launch = 100 * (start[0] - '0');
+        launch = launch + 10 * (start[1] - '0');
+        launch = launch + (start[2] - '0');
+        if (launch > 0 && launch <= check) {
+            free(start);
+            return launch;
+        } else {
+            system("clear");
+            printf("Неверный ввод, повторите попытку\n");
+            sleep(3);
+            free(start);
+            return -1;
+        }
+    }
+    if ((isdigit(start[0]) != 0) && (isdigit(start[1]) != 0)
+        && (start[2] == 0)) {
+        launch = launch + 10 * (start[0] - '0');
+        launch = launch + (start[1] - '0');
+        if (launch > 0 && launch <= check) {
+            free(start);
+            return launch;
+        } else {
+            system("clear");
+            printf("Неверный ввод, повторите попытку\n");
+            sleep(3);
+            free(start);
+            return -1;
+        }
+    }
+
+    if ((isdigit(start[0]) == 0) || start[1] != 0) {
         system("clear");
         printf("Неверный ввод, повторите попытку\n");
         sleep(3);
         free(start);
-        return 10;
+        return -1;
     }
     launch = start[0] - '0';
-    if (start[1] != 0) {
-        system("clear");
-        printf("Неверный ввод, повторите попытку\n");
-        sleep(3);
-        free(start);
-        return 10;
-    }
     if (launch > 0 && launch <= check) {
         free(start);
         return launch;
@@ -28,7 +54,7 @@ int check(int check)
         printf("Неверный ввод, повторите попытку\n");
         sleep(3);
         free(start);
-        return 10;
+        return -1;
     }
     free(start);
 }
