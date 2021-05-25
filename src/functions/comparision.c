9f2976fe1e87f49475b22ebeffdcc86ea8e38ed7
WhatIsLoveSib <fisherfawer@gmail.com>
2021-05-25 19:37:22 +0700

    Remove extra lines and fix codestyle

--- src/functions/comparision.c
+++ src/functions/comparision.c
@@ -2,9 +2,6 @@
 
 int comparison(int* gbe, int* mine, int* smse, int* price)
 {
-    printf("gbe = %d", *gbe);
-    printf("mine = %d", *mine);
-    printf("smse = %d", *smse);
     struct compareTariffs {
         char tname[25];
 
@@ -20,84 +17,80 @@ int comparison(int* gbe, int* mine, int* smse, int* price)
         char priceHead[35];
         int priceValue;
     };
-    
+
     int i;
     float gbPercent, minPercent, smsPercent, pricePercent;
-    //float minGbPercent, minMinPercent, minSmsPercent, minPricePercent;
 
     FILE* tariff;
 
     struct compareTariffs checking[16];
-    //char tariffs[4][8] = {{"MTC"}, {"TELE2"}, {"YOTA"}, {"MEGAFON"}};
     char path[30];
     char csv[4] = {1, 2, 3, 4};
-    //MTC
     for (i = 0; i < 4; i++) {
-            sprintf(path, "../csv_input/MTC/%d.csv", csv[i]);
-            tariff = fopen(path, "r");
-            fscanf(tariff,
-                "%s%s%d%s%d%s%d%s%d",
-                checking[i].tname,
-                checking[i].gbHead,
-                &checking[i].gbValue,
-                checking[i].minHead,
-                &checking[i].minValue,
-                checking[i].smsHead,
-                &checking[i].smsValue,
-                checking[i].priceHead,
-                &checking[i].priceValue);
-            fclose(tariff);
+        sprintf(path, "../csv_input/MTC/%d.csv", csv[i]);
+        tariff = fopen(path, "r");
+        fscanf(tariff,
+               "%s%s%d%s%d%s%d%s%d",
+               checking[i].tname,
+               checking[i].gbHead,
+               &checking[i].gbValue,
+               checking[i].minHead,
+               &checking[i].minValue,
+               checking[i].smsHead,
+               &checking[i].smsValue,
+               checking[i].priceHead,
+               &checking[i].priceValue);
+        fclose(tariff);
     }
-    //TELE2
     for (i = 4; i < 8; i++) {
-            sprintf(path, "../csv_input/TELE2/%d.csv", csv[i-4]);
-            tariff = fopen(path, "r");
-            fscanf(tariff,
-                "%s%s%d%s%d%s%d%s%d",
-                checking[i].tname,
-                checking[i].gbHead,
-                &checking[i].gbValue,
-                checking[i].minHead,
-                &checking[i].minValue,
-                checking[i].smsHead,
-                &checking[i].smsValue,
-                checking[i].priceHead,
-                &checking[i].priceValue);
-            fclose(tariff);
+        sprintf(path, "../csv_input/TELE2/%d.csv", csv[i - 4]);
+        tariff = fopen(path, "r");
+        fscanf(tariff,
+               "%s%s%d%s%d%s%d%s%d",
+               checking[i].tname,
+               checking[i].gbHead,
+               &checking[i].gbValue,
+               checking[i].minHead,
+               &checking[i].minValue,
+               checking[i].smsHead,
+               &checking[i].smsValue,
+               checking[i].priceHead,
+               &checking[i].priceValue);
+        fclose(tariff);
     }
 
     for (i = 8; i < 12; i++) {
-            sprintf(path, "../csv_input/MEGAFON/%d.csv", csv[i-8]);
-            tariff = fopen(path, "r");
-            fscanf(tariff,
-                "%s%s%d%s%d%s%d%s%d",
-                checking[i].tname,
-                checking[i].gbHead,
-                &checking[i].gbValue,
-                checking[i].minHead,
-                &checking[i].minValue,
-                checking[i].smsHead,
-                &checking[i].smsValue,
-                checking[i].priceHead,
-                &checking[i].priceValue);
-            fclose(tariff);
+        sprintf(path, "../csv_input/MEGAFON/%d.csv", csv[i - 8]);
+        tariff = fopen(path, "r");
+        fscanf(tariff,
+               "%s%s%d%s%d%s%d%s%d",
+               checking[i].tname,
+               checking[i].gbHead,
+               &checking[i].gbValue,
+               checking[i].minHead,
+               &checking[i].minValue,
+               checking[i].smsHead,
+               &checking[i].smsValue,
+               checking[i].priceHead,
+               &checking[i].priceValue);
+        fclose(tariff);
     }
 
     for (i = 12; i < 16; i++) {
-            sprintf(path, "../csv_input/YOTA/%d.csv", csv[i-12]);
-            tariff = fopen(path, "r");
-            fscanf(tariff,
-                "%s%s%d%s%d%s%d%s%d",
-                checking[i].tname,
-                checking[i].gbHead,
-                &checking[i].gbValue,
-                checking[i].minHead,
-                &checking[i].minValue,
-                checking[i].smsHead,
-                &checking[i].smsValue,
-                checking[i].priceHead,
-                &checking[i].priceValue);
-            fclose(tariff);
+        sprintf(path, "../csv_input/YOTA/%d.csv", csv[i - 12]);
+        tariff = fopen(path, "r");
+        fscanf(tariff,
+               "%s%s%d%s%d%s%d%s%d",
+               checking[i].tname,
+               checking[i].gbHead,
+               &checking[i].gbValue,
+               checking[i].minHead,
+               &checking[i].minValue,
+               checking[i].smsHead,
+               &checking[i].smsValue,
+               checking[i].priceHead,
+               &checking[i].priceValue);
+        fclose(tariff);
     }
 
     struct compareTariffs minRecommend;
@@ -109,32 +102,34 @@ int comparison(int* gbe, int* mine, int* smse, int* price)
             minRecommend.smsValue = 1000;
             minRecommend.minValue = 1000;
             minRecommend.priceValue = 1000;
-
         }
         gbPercent = ((float)*gbe / (float)checking[i].gbValue) * 100;
         minPercent = ((float)*mine / (float)checking[i].minValue) * 100;
         smsPercent = ((float)*smse / (float)checking[i].smsValue) * 100;
         pricePercent = ((float)*price / (float)checking[i].priceValue) * 100;
 
-        if ((gbPercent < 100) && (checking[i].gbValue < minRecommend.gbValue)) 
-        {
+        int different = minRecommend.priceValue - checking[i].priceValue;
+        if (different < 0) {
+            different *= -1;
+        }
+
+        if ((gbPercent < 100) && (checking[i].gbValue < minRecommend.gbValue)) {
             counter++;
         }
-        if ((minPercent < 100) && (checking[i].minValue < 
-            minRecommend.minValue)) {
+        if ((minPercent = 100)
+            && (checking[i].minValue < minRecommend.minValue)) {
             counter++;
         }
-        if ((smsPercent < 100) && (checking[i].smsValue < 
-            minRecommend.smsValue)) {
+        if ((smsPercent < 100)
+            && (checking[i].smsValue < minRecommend.smsValue)) {
             counter++;
         }
-        if ((pricePercent <= 100) && (checking[i].priceValue <= 
-            minRecommend.priceValue)) {
+        if ((pricePercent <= 100)
+            && (checking[i].priceValue < minRecommend.priceValue)) {
             counter++;
         }
-        
+
         if (counter == 4) {
-            printf("gbpercent = %f\n", gbPercent);
             minRecommend = checking[i];
             counter = 0;
             gbPercent = 0;
